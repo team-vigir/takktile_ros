af5bdff5fb13ce825d748a7788367503520ee397
Jackson Carter <cartejac@onid.oregonstate.edu>
2015-05-11 21:22:29 -0700

    Modification to use private parameters for proper launch and to avoid a couple of errors. Toggles power pin just fine.

--- src/takktile_ros/rpi/takk_electric_reset.py
+++ src/takktile_ros/rpi/takk_electric_reset.py
@@ -2,11 +2,10 @@
 # This code must be run with super user privileges since it touches physical hardware
 # Valuable Resources: http://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
 #	pi pinout diagram in takktile_ros catkin branch
-# TODO: Make this the keep-alive checker as well if possible
 import sys
 import rospy
 import RPi.GPIO as GPIO
-from takktile_ros.msg import Raw
+from takktile_ros.msg import Touch
 
 takk_data_flag = None	# States whether data has been received yet from takktile sensors in this check cycle
 switch_channel = 14 	# The pin used for toggling the takktile power circuit
@@ -16,6 +15,7 @@ def power_cycle_takk():
 	off_duration = get_off_duration()
 	
 	# Toggle the switch pin for a duration
+	rospy.logdebug("Toggling takktile sensors")
 	GPIO.output(switch_channel, GPIO.LOW)
 	rospy.sleep(off_duration)
 	GPIO.output(switch_channel, GPIO.HIGH)
@@ -31,7 +31,7 @@ def get_off_duration():
 
 def init_gpio():
 	global switch_channel
-	GPIO.setmode(GPIO.BOARD)
+	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(switch_channel, GPIO.OUT, initial=GPIO.HIGH)
 
 def takktile_data_cb(msg):
@@ -39,31 +39,36 @@ def takktile_data_cb(msg):
 	takk_data_flag = True
 
 if __name__ == "__main__":
-	global takk_data_flag
 	rospy.init_node("takk_electric_reset")
 
 	init_gpio()
-	
-	rospy.logerror("takktile reset node developed for left hand only. Set parameter server appropriately.")
-	hand= "left"
-	takktile_topic = "takktile/raw"
+	hand = rospy.get_param("~hand_side", "left")
+	takktile_topic = rospy.get_param("~takktile_reset_topic", "left_hand/tactile/calibrated")
+	#rospy.logerr("takktile reset node developed for left hand only. Set parameter server appropriately.")
+	#hand= "left"
+	#takktile_topic = "takktile/raw"
+
+	rospy.loginfo("Tactile reset enabled listening to " + takktile_topic + " for tactile updates on hand " + hand)
 
 	acceptable_takk_latency = rospy.Duration(1)
-	checkin_period = rospy.Duration(5)
+	checkin_period = rospy.Duration(4)
 	bringup_delay = rospy.Duration(10) #TODO: Determine an accurate bringup period on a fully loaded pi
 	while not rospy.is_shutdown():
 		# Subscribe and wait for a data msg
-		takk_data_sub = rospy.Subscribe(takktile_topic, Raw, takktile_data_cb)
+		takk_data_sub = rospy.Subscriber(takktile_topic, Touch, takktile_data_cb)
 		rospy.sleep(acceptable_takk_latency)
 		if takk_data_flag == False:
 			# Sensors are dead
 			power_cycle_takk()
 			#TODO: Kill the takktile_ros node?
+			rospy.logdebug("Sleeping bringup_delay seconds")
 			rospy.sleep(bringup_delay)
 
 		# Reset and wait
 		if takk_data_flag is not None:
 			takk_data_flag = False
-		takk_data_sub.unsubscribe()
+		else:
+			rospy.loginfo("No takktile data received")
+		takk_data_sub.unregister()
 		rospy.sleep(checkin_period)
 		
